5ff70c46c3e5b2ffb13d9b90eb5177b90b72a271
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-06 19:57:52 +0000

    Add src from previous week's lesson with the navbar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo world</h1>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes basename="second-portfolio-with-react">
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="second-portfolio-with-react/" element={<Home />} />
+          <Route path="second-portfolio-with-react/about" element={<About />} />
+          <Route path="second-portfolio-with-react/blog" element={<Blog />} />
+          {/* Define a route that will have descendant routes */}
+          <Route
+            path="second-portfolio-with-react/contact/*"
+            element={<Contact />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavTabs.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="second-portfolio-with-react/"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="second-portfolio-with-react/about"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="second-portfolio-with-react/blog"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Blog
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="second-portfolio-with-react/contact"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="second-portfolio-with-react/contact/learn"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Learn
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
