8491e2349c222d64eed7da3cf2832b0e4a6f1c62
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-09 11:12:34 +0000

    Add treeinlake pic as a standard background for entire site

--- src/App.js
+++ src/App.js
@@ -5,27 +5,40 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
+import Header from "./components/Header";
 
 function App() {
+  const backgroundStyle = {
+    backgroundImage: "url(./treeinlake.png)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+  };
+
   return (
-    <Router>
-      <div>
-        <NavTabs />
-        {/* Wrap Route elements in a Routes component */}
-        <Routes basename="second-portfolio-with-react">
-          {/* Define routes using the Route component to render different page components at different paths */}
-          {/* Define a default route that will render the Home component */}
-          <Route path="second-portfolio-with-react/" element={<Home />} />
-          <Route path="second-portfolio-with-react/about" element={<About />} />
-          <Route path="second-portfolio-with-react/blog" element={<Blog />} />
-          {/* Define a route that will have descendant routes */}
-          <Route
-            path="second-portfolio-with-react/contact/*"
-            element={<Contact />}
-          />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div style={backgroundStyle}>
+          <Header />
+          {/* Wrap Route elements in a Routes component */}
+          <Routes basename="second-portfolio-with-react">
+            {/* Define routes using the Route component to render different page components at different paths */}
+            {/* Define a default route that will render the Home component */}
+            <Route path="second-portfolio-with-react/" element={<Home />} />
+            <Route
+              path="second-portfolio-with-react/about"
+              element={<About />}
+            />
+            <Route path="second-portfolio-with-react/blog" element={<Blog />} />
+            {/* Define a route that will have descendant routes */}
+            <Route
+              path="second-portfolio-with-react/contact/*"
+              element={<Contact />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(<App />);
