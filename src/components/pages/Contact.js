aad9bb87d7b2cf232324bff8608673eb0c19cf41
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-09 12:01:12 +0000

    Remove CV pdf and add a link to it

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Learn from "./Learn";
+import { PDF } from "react-embed-pdf";
 
 function Contact(props) {
   return (
@@ -14,6 +15,11 @@ function Contact(props) {
       </p>
       <p>
         <a href="https://www.github.com/dantrovato">GitHub</a>
+      </p>
+      <p>
+        <a href="https://drive.google.com/file/d/1d_f5eUS48OImEFJ62ihSVMTalcgzvqq7/view?usp=sharing">
+          CV
+        </a>
       </p>
       <Link to="learn" role="button" className="btn btn-link">
         Learn More
